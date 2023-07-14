8e92c15054ad9774a75c5d08ed8a29d197e1df81
Jaush-M <jaushvx@icloud.com>
2023-07-14 17:50:10 +0500

    feat: define product category controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductCategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return to_route('product-categories.index');
 });
+
+Route::resource('product-categories', ProductCategoryController::class);
