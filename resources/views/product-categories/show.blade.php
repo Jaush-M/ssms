7a1089dcb006944891a0ee999c7d653476ae71ea
Jaush-M <jaushvx@icloud.com>
2023-07-14 18:58:17 +0500

    fix: 🌙 dark mode

--- resources/views/product-categories/show.blade.php
+++ resources/views/product-categories/show.blade.php
@@ -12,7 +12,7 @@
       <h1 class='text-6xl font-bold tracking-tight'>{{ $product_category->name }}</h1>
       <p class='badge rounded-lg bg-violet-700 py-4 px-4 text-xs text-white'>{{ $product_category->code }}</p>
     </div>
-    <p class='max-w-lg text-sm text-gray-800'>
+    <p class='max-w-lg text-sm'>
       Et quo unde explicabo reiciendis natus exercitationem dolores perferendis consequuntur soluta omnis voluptatibus
       dicta, quisquam voluptate itaque eos obcaecati, nihil architecto autem! Vi, sit amet consectetur
       adipisicing elit. Cupiditate maxime fugiat nisi alias animi esse eum laboriosam.
